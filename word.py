5cb10d741d75237e581ade5dc410044a4e81458c
GriMel <GriefMontana@gmail.com>
2016-02-19 12:23:13 +0200

    Removed unneeded 'super'

--- word.py
+++ word.py
@@ -42,7 +42,6 @@ class Kindle(Base):
         All words - category = 100.
         New words - category = 0.
         """
-        super(Kindle, self).read()
         conn = sqlite3.connect(self.source)
         command = None
         if only_new_words:
