483eca15fb2f850a2e77cfcc019efe094bd088e6
GriMel <GriefMontana@gmail.com>
2016-02-23 23:48:42 +0200

    Service coverage -> 95%

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -11,7 +11,7 @@ from service import Lingualeo
 from collections import Counter
 import sqlite3
 import os
-
+import json
 
 def createKindleDatabase(db_name):
     """
@@ -119,6 +119,179 @@ class TestLingualeo(unittest.TestCase):
     """
     Ensure that Lingualeo API is still the same
     """
+
+    EMAIL = "b346059@trbvn.com"
+    PASSWORD = "1234567890"
+    TEST_AUTH_INFO = {'address': 'Ukraine, Kyyiv, Kiev',
+        'age': 16,
+        'autologin_key': 'd3c12598553e65d50ba52e1abcb5c705',
+        'avatar': 'https://d144fqpiyasmrr.cloudfront.net/up'\
+                  'loads/avatar/0.png',
+        'avatar_mini': 'https://d144fqpiyasmrr.cloudfront.ne'\
+                 't/uploads/avatar/0s100.png',
+        'birth': '2000-01-01',
+        'create_at': '2016-02-19 20:33:54',
+        'daily_hours': 1,
+        'denied_services': [],
+        'fname': '',
+        'fullname': 'b346059',
+        'hungry_max_points': 150,
+        'hungry_pct': 0,
+        'hungry_points': 0,
+        'is_gold': False,
+        'lang_interface': 'ru',
+        'lang_native': 'ru',
+        'langlevel': 1,
+        'leo_pic_url': 'http://d29idiqnj27ux5.cloudfront.net'\
+                       '/8ac94ea62/images/tasks-leo-2.png',
+        'meatballs': 200,
+        'nickname': 'b346059',
+        'port_version': '1.6.1',
+        'premium_type': 0,
+        'premium_until': '',
+        'refcode': '9em5gg',
+        'sex': 1,
+        'sname': '',
+        'user_id': 13720532,
+        'words_cnt': 30,
+        'words_known': 0,
+        'xp_level': 1,
+        'xp_max_points': 25,
+        'xp_min_points': 0,
+        'xp_points': 0,
+        'xp_title': 'Ловкий новичок'}
+    TEST_ADD_INFO = {'added_translate_count': 0,
+                     'error_msg': '',
+                     'from_syntrans_id': None,
+                     'is_new': 0,
+                     'is_user_word_created': 0,
+                     'lang': {'current': 'ru', 'target': 'en'},
+                     'lemmas': [{'lemma_id': 5756,
+                     'lemma_value': 'BOOK',
+                     'speech_part': {
+                        'code': 'Adjective',
+                        'name': 'прилагательное',
+                        'short_name': 'прил.'
+                        },
+                    'speech_part_id': 1},
+                    {
+                        'lemma_id': 5756,
+                        'lemma_value': 'BOOK',
+                        'speech_part': {
+                            'code': 'Noun',
+                            'name': 'существительное',
+                            'short_name': 'сущ.'
+                            },
+                    'speech_part_id': 7},
+                    {
+                        'lemma_id': 5756,
+                        'lemma_value': 'BOOK',
+                        'speech_part': {
+                            'code': 'Verb',
+                            'name': 'глагол',
+                            'short_name': 'глаг.'
+                            },
+                    'speech_part_id': 12}],
+                    'sound_url': 'http://d2x1jgnvxlnz25.'\
+                                 'cloudfront.net/v2/3/5756-631152008.mp3',
+                    'speech_part_id': 0,
+                    'to_syntrans_id': None,
+                    'translate_auto': 0,
+                    'translate_id': 201656,
+                    'translate_value': 'книга',
+                    'word_id': 5756,
+                    'word_rank': 318585,
+                    'word_speech_parts': {
+                        '1': {
+                            'code': 'Adjective',
+                            'name': 'прилагательное',
+                            'short_name': 'прил.'
+                            },
+                        '12': {
+                            'code': 'Verb',
+                            'name': 'глагол',
+                            'short_name': 'глаг.'
+                            },
+                        '7': {
+                            'code': 'Noun',
+                            'name': 'существительное',
+                            'short_name': 'сущ.'}},
+                    'word_top': 1,
+                    'word_type': 1,
+                    'word_user_cdate': 1455914074,
+                    'word_value': 'book'}
+
+    TEST_WORD_EXISTS = "book"
+    TEST_TWORD_EXISTS = "книга"
+    TEST_TWORD_EXISTS_TWO = "бронировать"
+    TEST_WORD_NON_EXISTS = "zecrvt"
+
+    def setUp(self):
+        """
+        Authenticate test user
+        """
+        self.lingualeo = Lingualeo(self.EMAIL, self.PASSWORD)
+        self.lingualeo.auth()
+        self.maxDiff = None
+
+    def test_typical_auth_info(self):
+        """
+        Auth_info is equal to test auth info
+        """
+        self.assertEqual(self.lingualeo.auth_info, self.TEST_AUTH_INFO)
+
+    def test_typical_get_translation_info(self):
+        """
+        Function get_translate returns predicted results
+        """
+        result = self.lingualeo.get_translate(self.TEST_WORD_EXISTS)
+        # Dictionary on Lingualeo has 'бронировать' for 'book' 
+        # The most popular translation, though, is 'книга'
+        self.assertEqual(result,
+                         {'is_exist': True,
+                          'word':"book",
+                          'tword': self.TEST_TWORD_EXISTS})
+        result = self.lingualeo.get_translate(self.TEST_WORD_NON_EXISTS)
+        self.assertEqual(result,
+                         {'is_exist': False,
+                          'word':"zecrvt",
+                          'tword': ""})
+
+    def test_typical_add_word_info(self):
+        """
+        Function add_word returns predicted results
+        """
+        response = self.lingualeo.add_word(self.TEST_WORD_EXISTS,
+                                                self.TEST_TWORD_EXISTS)
+        add_word_info = response.json()
+        self.assertEqual(self.TEST_ADD_INFO, add_word_info)
+
+    def test_init_info(self):
+        """
+        Test user has no premium status
+        """
+        self.lingualeo.initUser()
+        self.assertEqual(self.lingualeo.premium, 0)
+        self.assertEqual(self.lingualeo.fname, 'b346059')
+        self.assertEqual(self.lingualeo.lvl, 1)
+        self.assertEqual(self.lingualeo.meatballs, 200)
+
+    def test_enough_meatballs(self):
+        """
+        Meatballs < words returns False
+        """
+        self.lingualeo.initUser()
+        result = self.lingualeo.isEnoughMeatballs(201)
+        self.assertEqual(result, False)
+
+    def test_avatar_exception(self):
+        """
+        If no avatar - avatar is set to None
+        """
+        self.lingualeo.TIMEOUT = 0.01
+        self.lingualeo.initUser()
+        self.assertEqual(self.lingualeo.avatar, None)
+
 
 
 class TestKindleHandler(unittest.TestCase):
